26534531715a9bf571dbcaa2d3bd383d302e9e9b
kaungmyatpaing21 <kaungmyatpaing2110@gmail.com>
2019-11-18 22:56:46 +0630

    injecting literal values

--- src/main/java/com/luv2code/SpringDemo/CricketCoach.java
+++ src/main/java/com/luv2code/SpringDemo/CricketCoach.java
@@ -2,10 +2,30 @@ package com.luv2code.SpringDemo;
 
 public class CricketCoach implements Coach {
 
-    FortuneService fortuneService;
+    private FortuneService fortuneService;
+    private String emailAddress;
+    private String team;
 
     public CricketCoach(){
         System.out.println("inside the no-arg constructor.");
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        System.out.println("inside email setter method.");
+        this.emailAddress = emailAddress;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("inside team setter method.");
+        this.team = team;
     }
 
     @Override
--- src/main/java/com/luv2code/SpringDemo/SetterDemoApp.java
+++ src/main/java/com/luv2code/SpringDemo/SetterDemoApp.java
@@ -4,11 +4,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SetterDemoApp {
     public static void main(String[] args) {
+
         ClassPathXmlApplicationContext theContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Coach theCoach = theContext.getBean("myCricketCoach", Coach.class);
+        CricketCoach theCoach = theContext.getBean("myCricketCoach", CricketCoach.class);
         System.out.println(theCoach.getDailyWorkout());
         System.out.println(theCoach.getFortuneService());
+        System.out.println(theCoach.getEmailAddress());
+        System.out.println(theCoach.getTeam());
         theContext.close();
-
     }
 }
